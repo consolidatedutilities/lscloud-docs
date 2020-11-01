015c195b9ace28b72986cafa1f6e9656a8f76c92
Brian Abbott <burnedfaceless@gmail.com>
2020-11-01 11:31:21 -0500

    Created blank page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import logo from './Combined.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="logo" alt="Logo" />
+      <p>
+        This website will contain documentation for the LiftStation.cloud app. 
+      </p>
     </div>
   );
 }
